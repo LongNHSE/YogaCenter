a91f36c73cea1f25e10d027241d887416bd2d35e
Oalskad <huylong2113@gmail.com>
2023-05-19 15:25:03 +0700

    DTO cua Admin

---
+++ src/main/java/com/mycompany/yogacenterproject/dto/AdminDTO.java
@@ -0,0 +1,14 @@
+package com.mycompany.yogacenterproject.dto;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Oalskad
+ */
+public class AdminDTO {
+    
+}
